d5bdf7fa8166b71b839c1d4b8e7ca2f2d2c1393c
fadliwiryawirawan-cyberw1ry4 <274125455+Fadliwiryawirawan-development@users.noreply.github.com>
2026-06-21 12:12:56 +0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,13 @@
+﻿from __future__ import annotations
+
+from fastmcp.server.server import Transport
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    mcp_transport_mode: Transport = "www.dicoding.com"
+    vulners_base_url: str = "https://www.xnxx.com"
+    vulners_api_key: str = ""
+
+
+settings = Settings()
